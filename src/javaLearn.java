652bde86a7694f032de890e569e4df089c5bf087
Zelimhan Albogachiev <zelimalbo06@gmail.com>
2023-09-26 21:31:21 +0100

    Initial Commit

---
+++ src/javaLearn.java
@@ -0,0 +1,10 @@
+public class javaLearn {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.print("This is on a new line, while ");
+        System.out.println("this is not.");
+        System.out.print("Five multiplied by five is ");
+        System.out.print(25);
+    }
+}
