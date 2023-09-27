5f8691b282c6ce726aa4d52076e44e12d804d4b7
Zelimhan Albogachiev <zelimalbo06@gmail.com>
2023-09-27 12:00:58 +0100

    javalearn.java

--- src/javaLearn.java
+++ src/javaLearn.java
@@ -8,6 +8,33 @@ public class javaLearn {
         System.out.println(25);
         int a = 17;
                 System.out.println(a);
-                
+        String firstName = "Zelimhan ";
+        String lastName = "Albogachiev";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+
+        byte Seventy = 70;
+        System.out.println(Seventy);
+
+        long veryLong = 100000000000L;
+        System.out.println(veryLong);
+
+        boolean amIeighteen = true;
+        boolean amInineteen = false;
+        System.out.println(amIeighteen);
+        System.out.println(amInineteen);
+
+        int singleDigit = 7;
+        double doubleDigit = singleDigit;
+        System.out.println(singleDigit);
+        System.out.println(doubleDigit);
+
+        double doubleDigitTwo = 7.45d;
+        int singleDigitTwo = (int) doubleDigitTwo;
+        System.out.println(doubleDigitTwo);
+        System.out.println(singleDigitTwo);
     }
 }
